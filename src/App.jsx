15a76946cf996aa37b31f35c331785e7a94b6e6f
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-02-02 18:22:53 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import Statistics from 'components/Statistics/Statistics';
 import  Profile  from './components/Profile/Profile';
+import user from './components/Profile/user.json'
+
+// import Statistics from 'components/Statistics/Statistics';
+
 export const App = () => {
   return (
     <>
-     <Profile />
-     <Statistics />
+     <Profile 
+     avatar={user.avatar}
+     username={user.username}
+     tag={user.tag}
+     location={user.location}
+     stats={user.stats}
+     />
+     {/* <Statistics /> */}
     </>
   );
 };
